a8b533b488fffdf2926cf8c96b5890dabfa502d7
rish-0-0 <rish-0-0@users.noreply.github.com>
2024-10-28 21:54:40 +0000

    deploy: 7e3def6bd2851c22ebb191e7ea9e18e750f466d4

---
+++ assets/NeoFlowViz-CZSIQEa6.js
@@ -0,0 +1,19 @@
+import{j as e}from"./_commonjs-dynamic-modules-O7U1px_y.js";import{useMDXComponents as s}from"./index-BrnU7xv7.js";import{ae as i,af as r}from"./index-B5PoY6p_.js";import{N as a,B as d}from"./NeoFlowViz.stories-BsQS6gJL.js";import"./index-DJO9vBfz.js";import"./iframe-BunTdtXE.js";import"../sb-preview/runtime.js";import"./index-rCr475hU.js";import"./index-D-8MO0q_.js";import"./index-CvTBScqg.js";import"./index-DrFu-skq.js";function n(o){return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:a}),`
+`,e.jsx("h1",{children:"Code Examples"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("h2",{children:"Getting Started"}),`
+`,e.jsx(r,{dark:!0,of:d}),`
+`,e.jsx("h2",{children:"Complete Example"}),`
+`,e.jsx(r,{dark:!0,code:`
+  <NeoFlowViz
+    query={query} // CYPHER Query
+    response={response} // NodeJS style Neo4j Raw Response (checkout neo4j-server directory for an example)
+    width={1500} // width
+    height={800} // height
+    colorMode="system" // dark, light, system
+    direction="DOWN" // direction of ELKJS based layout
+    onNodeClick={(event, node) => console.log({node})} // callback
+    onEdgeClick={(event, edge) => console.log({edge})} // callback
+    showDetails={true} // shows the panel with the properties on the right side
+  />
+`})]})}function y(o={}){const{wrapper:t}={...s(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n()}export{y as default};
